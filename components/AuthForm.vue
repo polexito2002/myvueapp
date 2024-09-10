e4fe3541b1634a2db3c357716d588489ea0ffafd
polexito2002 <56658279+polexito2002@users.noreply.github.com>
2024-09-10 13:29:51 -0500

    segundo commit

--- components/AuthForm.vue
+++ components/AuthForm.vue
@@ -3,13 +3,28 @@
     <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
       <div class="sm:mx-auto sm:w-full sm:max-w-sm">
         <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" />
-        
+        <Heading as="h2">
+         <span v-if="props.formType == 'signin'">
+          Sign in to your account
+         </span>
+         <span  v-else>
+          Create a new account
+         </span>
+        </Heading>
       </div>
   
       <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
         <form class="space-y-6" action="#" method="POST">
          
 
+            <FormGroup v-if="props.formType == 'signup'"  
+                        label="Full name"  
+                        name="name" type="text" 
+                        autocomplete="name" 
+                        required labelFor="name" 
+                        labelClass="block text-sm font-medium leading-6 text-gray-900"
+                        inputClass="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        />
             <FormGroup label="Email address"  
                         name="email" type="email" 
                         autocomplete="email" 
@@ -26,17 +41,36 @@
                         required labelFor="password"
                         />
          
-              <div class="text-sm">
+              <div class="text-sm text-end" v-if="props.formType == 'signin'">
                 <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
               </div>
             </div>
          
             <div>
-            <Button type=submit>Sign In</Button>
+            <Button type=submit>
+              <span v-if="props.formType == 'signin' " >
+              Sign In
+              </span>
+              <span v-else>
+                Sigup
+              </span>
+              </Button>
           </div>
         </form>
   
       </div>
     </div>
   </template>
+
+
+  <script setup>
+
+const props = defineProps({
+formType: {
+  type: String,
+  default: 'signin',
+  validator: prop => ['signin','signup'].includes(prop)
+}
+})
+  </script>
   
--- components/Heading.vue
+++ components/Heading.vue
@@ -1,3 +1,47 @@
 <template>
-    <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
+    <component :is="as" :class="`text-center leading-9 tracking-tight ${as}`">
+    <slot></slot>
+    </component>
 </template>
+
+<script setup>
+
+defineProps({
+as: {
+    type:String,
+    default: 'h2',
+    validator: (value) => ['h1','h2','h3','h4','h5','h6'].includes(value)
+}
+
+})
+</script>
+
+<style scoped>
+
+.h1 {
+    @apply text-4xl font-bold text-gray-900
+}
+.h2 {
+    @apply text-2xl font-bold text-gray-900
+    
+}
+.h3 {
+    @apply text-xl font-bold text-gray-900
+
+}
+.h4 {
+    @apply text-lg font-bold text-gray-900
+
+}
+.h5 {
+    @apply text-base font-bold text-gray-900
+
+}
+.h6 {
+    @apply text-sm font-bold text-gray-900
+
+}
+
+
+
+</style>
